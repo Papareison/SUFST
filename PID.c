24cefe676b4b9eb780dcc1f8d428e42ad714d8e1
Papareison <147756476+Papareison@users.noreply.github.com>
2023-12-11 17:23:50 +0000

    TODO msg removed

--- PID.c
+++ PID.c
@@ -8,9 +8,9 @@ int main(){
     double previous_error = 0;
     double idealratio = 0.1;
     double ratio = 0.2;
-    
+
     double PC = 0.75;  // Proportional coefficient    // These need to be ORDERS of magnitude smaller than the ideal ratio and definitely not larger
-    double IC = 1;  // Integral coefficient       // ***TODO***: Tune the coefficients to ensure optimal behavior
+    double IC = 1;  // Integral coefficient
     double DC = 0.0001; // Derivative coefficient
     double dt = 0.001; // Time step
 
